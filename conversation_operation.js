e6935e5b35b50d2241d433e011081b8b638e2f01
Sauravn04 <123154757+Sauravn04@users.noreply.github.com>
2024-01-19 13:04:03 +0000

    container config done

---
+++ conversation_operation.js
@@ -0,0 +1,30 @@
+let score="30abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueinnumber=Number(score);
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+// Data type conversion
+// 30 = 30
+// 30abc = NaN
+// true = 1
+// false = 0
+
+let loggedin="Saurav"
+
+let booleanisloggedin = Boolean(loggedin)
+console.log(booleanisloggedin);
+
+// 1 = true 
+// 0 = false
+// "" = false
+// "saurav" = true
+
+let somenumber=23
+
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
---
+++ variables.js
@@ -0,0 +1,8 @@
+const accountid = 144553
+let accountemail = "nayaksaurav99@gmail.com"
+var password = "123456"
+accountcity = "pune"
+let accountstate
+
+console.log(accountid);
+console.table([accountid, accountemail, password, accountcity, accountstate]); 
